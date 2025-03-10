7545fdf371a1b0ea00d5151a7fc0d90fdcdcf09f
rlawnstn5076 <nieRmocha4724@gmail.com>
2025-03-10 22:17:47 +0900

    hw1

--- WebGLSource/01_HelloWindow/01_HelloWindow.js
+++ WebGLSource/01_HelloWindow/01_HelloWindow.js
@@ -7,27 +7,64 @@ if (!gl) {
 }
 
 // Set canvas size: 현재 window 전체를 canvas로 사용
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = 500;
+canvas.height = 500;
 
 // Initialize WebGL settings: viewport and clear color
 gl.viewport(0, 0, canvas.width, canvas.height);
-gl.clearColor(0.1, 0.2, 0.3, 1.0);
+
+
+
+
+
+
+
+
+
 
 // Start rendering
 render();
 
 // Render loop
 function render() {
-    gl.clear(gl.COLOR_BUFFER_BIT);    
-    // Draw something here
+    gl.enable(gl.SCISSOR_TEST);
+
+    //Left bottom
+    gl.scissor(0, 0, canvas.width/2, canvas.height/2);
+    gl.clearColor(0, 0.4, 0.7, 1);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    //Left top
+    gl.scissor(0, canvas.height/2, canvas.width/2, canvas.height/2);
+    gl.clearColor(1, 0, 0, 1);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    //Right bottom
+    gl.scissor(canvas.width/2, 0, canvas.width/2, canvas.height/2);
+    gl.clearColor(1, 1, 0, 1);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    //Right top
+    gl.scissor(canvas.width/2, canvas.height/2, canvas.width/2, canvas.height/2);
+    gl.clearColor(0, 0.6, 0.4, 1);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+
+    gl.disable(gl.SCISSOR_TEST);
+
 }
 
 // Resize viewport when window size changes
 window.addEventListener('resize', () => {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+    shorterLength = 500;
+    if (window.innerWidth > window.innerHeight) {
+        shorterLength = window.innerHeight;
+    } else {
+        shorterLength = window.innerWidth;
+    }
+    canvas.width = shorterLength;
+    canvas.height = shorterLength;
     gl.viewport(0, 0, canvas.width, canvas.height);
-    render();
+    render();   
 });
 
